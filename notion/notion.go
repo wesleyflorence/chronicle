1bc9d36c51f3385960bfe0e0c3155928e83d2bdb
Wesley Florence <wesleyflorence@Wesleys-Mac-Studio.local>
2023-09-30 17:23:37 -0700

    Adding to the digestion db

--- notion/notion.go
+++ notion/notion.go
@@ -3,41 +3,79 @@ package notion
 
 import (
 	"context"
-	"log"
-	"os"
+	"strconv"
+	"time"
 
-	"github.com/joho/godotenv"
 	"github.com/jomei/notionapi"
 )
 
-const testDbID = "632816ea9920439dbd1019c91fc5fa15"
-
-func getAPIKey() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+// AppendDigestionEntry adds a digestion entry to a Notion database.
+//
+// It creates a new page with the provided digestion entry details,
+// including the Bristol scale, size, notes, and other properties.
+func AppendDigestionEntry(client *notionapi.Client, dbID string, bristol int, size, notes string) (*notionapi.Page, error) {
+	currentTime := notionapi.Date(time.Now())
+	props := notionapi.Properties{
+		"Week": notionapi.TitleProperty{
+			Title: []notionapi.RichText{
+				{Text: &notionapi.Text{Content: weekRelativeToChemoStart(time.Now())}},
+			},
+		},
+		"Type": notionapi.MultiSelectProperty{
+			MultiSelect: []notionapi.Option{
+				{Name: "Poop", Color: notionapi.ColorBlue},
+			},
+		},
+		"Date": notionapi.DateProperty{
+			Date: &notionapi.DateObject{
+				Start: &currentTime,
+			},
+		},
+		"Bristol (1-7)": notionapi.NumberProperty{
+			Number: float64(bristol),
+		},
+		"Size": notionapi.SelectProperty{
+			Select: notionapi.Option{
+				Name:  size,
+				Color: lookupSizeColor(size),
+			},
+		},
+		"Notes": notionapi.RichTextProperty{
+			RichText: []notionapi.RichText{
+				{Text: &notionapi.Text{Content: notes}},
+			},
+		},
 	}
-	return os.Getenv("NOTION_API_KEY")
+	request := buildPageCreateRequest(dbID, &props)
+	return client.Page.Create(context.Background(), &request)
 }
 
-func addRowToDB() (*notionapi.Page, error) {
-	client := notionapi.NewClient(notionapi.Token(getAPIKey()))
-	request := notionapi.PageCreateRequest{
+func buildPageCreateRequest(parentID string, properties *notionapi.Properties) notionapi.PageCreateRequest {
+	return notionapi.PageCreateRequest{
 		Parent: notionapi.Parent{
-			DatabaseID: notionapi.DatabaseID(testDbID),
+			DatabaseID: notionapi.DatabaseID(parentID),
 		},
-		Properties: notionapi.Properties{
-			"Name": notionapi.TitleProperty{
-				Title: []notionapi.RichText{
-					{Text: &notionapi.Text{Content: "test name from golang"}},
-				},
-			},
-			"Tags": notionapi.MultiSelectProperty{
-				MultiSelect: []notionapi.Option{
-					{Name: "done", Color: notionapi.ColorPurple},
-				},
-			},
-		},
+		Properties: *properties,
 	}
-	return client.Page.Create(context.Background(), &request)
+}
+
+// Return the week relative to August 21st
+func weekRelativeToChemoStart(date time.Time) string {
+	startDate := time.Date(date.Year(), time.August, 21, 0, 0, 0, 0, time.UTC)
+	daysDiff := date.Sub(startDate).Hours() / 24
+	weeksDiff := int(daysDiff)/7 + 1
+	return strconv.Itoa(weeksDiff)
+}
+
+func lookupSizeColor(size string) notionapi.Color {
+	switch size {
+	case "Tiny":
+		return notionapi.ColorPink
+	case "Small":
+		return notionapi.ColorRed
+	case "Medium":
+		return notionapi.ColorOrange
+	default:
+		return notionapi.ColorYellow
+	}
 }
