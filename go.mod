444b5f3105bce29fd27bb56e513ef194bef43f18
Wesley Florence <wesleyflorence@Wesleys-Mac-Studio.local>
2023-10-01 19:24:18 -0700

    Starting work on login
    Need to make it single page next and honor the jwt

--- go.mod
+++ go.mod
@@ -3,18 +3,21 @@ module github.com/wesleyflorence/chronicle
 go 1.21.0
 
 require (
+	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/gofiber/contrib/jwt v1.0.7 // indirect
 	github.com/gofiber/fiber/v2 v2.49.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/jomei/notionapi v1.12.9 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.49.0 // indirect
+	github.com/valyala/fasthttp v1.50.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,12 +4,21 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
 	"github.com/jomei/notionapi"
 	"github.com/wesleyflorence/chronicle/notion"
 )
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln("Could not load env vars")
+	}
+}
 
 func main() {
 	app := fiber.New()
@@ -17,16 +26,66 @@ func main() {
 	log.Fatal(app.Listen(":3000"))
 }
 
+func setupUsers() map[string]string {
+	username := os.Getenv("CHRONICLE_USERNAME")
+	password := os.Getenv("CHRONICLE_PW")
+	users := map[string]string{
+		username: password,
+	}
+	return users
+}
+
 func setupRoutes(app *fiber.App) {
-	godotenv.Load()
 	notionAPIKey := os.Getenv("NOTION_API_KEY")
 	digestionDbID := os.Getenv("DIGESTION_DB")
 	medicinePageID := os.Getenv("MEDICINE_PAGE")
 	client := notionapi.NewClient(notionapi.Token(notionAPIKey))
 
-	app.Get("/", hello)
+	app.Static("/", "./public")
+	app.Post("/api/v1/login", handleLogin())
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte("secret")},
+	}))
+	app.Get("/api/v1/test", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
 	app.Post("/api/v1/dig", handleDigestionEntry(client, digestionDbID))
 	app.Post("/api/v1/med", handleMedicineEntry(client, medicinePageID))
+}
+
+func handleLogin() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		type Payload struct {
+			Password string
+		}
+		var payload Payload
+		if err := c.BodyParser(&payload); err != nil {
+
+			return err
+		}
+
+		if payload.Password != "test" {
+			return c.SendStatus(fiber.StatusUnauthorized)
+		}
+
+		// Create the Claims
+		claims := jwt.MapClaims{
+			"name":  "Wesley",
+			"admin": true,
+			"exp":   time.Now().Add(time.Hour * 72).Unix(),
+		}
+
+		// Create token
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+		// Generate encoded token and send it as response.
+		t, err := token.SignedString([]byte("secret"))
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		return c.JSON(fiber.Map{"token": t, "success": true})
+	}
 }
 
 func handleMedicineEntry(client *notionapi.Client, medicinePageID string) fiber.Handler {
