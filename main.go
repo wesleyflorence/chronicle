9806e05bd34247c643196bd5df4eba0c287df399
Wes Florence <hello@wesflorence.com>
2023-10-11 18:57:59 -0700

    cleaning up a bit

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ import (
 
 const admin = "wesley"
 
-var tokenAuth = jwtauth.New("HS256", []byte("secret"), nil)
+var tokenAuth *jwtauth.JWTAuth
 var tmpl *template.Template
 
 func init() {
@@ -39,7 +39,6 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	// Parse templates
 	var err error
 	tmpl, err = parseTemplates("views")
 	if err != nil {
@@ -47,14 +46,11 @@ func main() {
 	}
 
 	setupRoutes(r)
-
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-// parseTemplates will parse all templates in the specified folder and return a *template.Template
 func parseTemplates(folder string) (*template.Template, error) {
-	// Create a new template and parse the template definitions in the specified folder
-	return template.ParseGlob(filepath.Join(folder, "*.html")) // assuming your templates have a .html extension
+	return template.ParseGlob(filepath.Join(folder, "*.html"))
 }
 
 func setupUsers() map[string]string {
@@ -70,57 +66,38 @@ func setupRoutes(r *chi.Mux) {
 	notionAPIKey := os.Getenv("NOTION_API_KEY")
 	digestionDbID := os.Getenv("DIGESTION_DB")
 	medicinePageID := os.Getenv("MEDICINE_PAGE")
+	jwtSecret := os.Getenv("JWT_SECRET")
+	tokenAuth = jwtauth.New("HS256", []byte(jwtSecret), nil)
 	users := setupUsers()
 	client := notionapi.NewClient(notionapi.Token(notionAPIKey))
 
 	workDir, _ := os.Getwd()
 	filesDir := http.Dir(filepath.Join(workDir, "public"))
-	FileServer(r, "/", filesDir)
+	fileServer(r, "/", filesDir)
 
+	// Public routes
 	r.Group(func(r chi.Router) {
-		// Public routes
 		r.Get("/", handleHome)
-		r.Post("/api/v1/login", handleLogin(users)) // Pass the users map to the login handler
-		r.Get("/logout", func(w http.ResponseWriter, r *http.Request) {
-			// Clear the jwt cookie
-			http.SetCookie(w, &http.Cookie{
-				Name:     "jwt",
-				Value:    "",
-				Path:     "/",
-				Expires:  time.Unix(0, 0),
-				HttpOnly: true,
-			})
-
-			// For the HX-Redirect to work with htmx, you'll likely need to set it as a response header
-			w.Header().Set("HX-Redirect", "/")
-			w.Write([]byte("Logged out"))
-		})
-
+		r.Get("/logout", handleLogout)
+		r.Post("/api/v1/login", func(w http.ResponseWriter, r *http.Request) {
+			handleLogin(w, r, users)})
 	})
 
 	// Protected routes
 	r.Group(func(r chi.Router) {
-		// Seek, verify and validate JWT tokens
 		r.Use(jwtauth.Verifier(tokenAuth))
-
-		// Handle valid / invalid tokens. This can be modified
-		// based on the requirements of your application.
 		r.Use(jwtauth.Authenticator)
-
-		// Now define the routes that require a valid JWT
 		r.Post("/api/v1/dig", func(w http.ResponseWriter, r *http.Request) {
 			handleDigestionEntry(w, r, client, digestionDbID)
 		})
-
 		r.Post("/api/v1/med", func(w http.ResponseWriter, r *http.Request) {
 			handleMedicineEntry(w, r, client, medicinePageID)
 		})
 	})
 }
 
-// FileServer conveniently sets up a http.FileServer handler to serve
-// static files from a http.FileSystem.
-func FileServer(r chi.Router, path string, root http.FileSystem) {
+// Serve Static Files
+func fileServer(r chi.Router, path string, root http.FileSystem) {
 	if strings.ContainsAny(path, "{}*") {
 		panic("FileServer does not permit any URL parameters.")
 	}
@@ -140,81 +117,83 @@ func FileServer(r chi.Router, path string, root http.FileSystem) {
 }
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	var name string
 	isLoggedIn := false
 	cookie, _ := r.Cookie("jwt")
 	if cookie != nil {
-		token, _ := tokenAuth.Decode(cookie.Value)
-		name, _ := token.Get("name")
-		if name == admin {
-			isLoggedIn = true
+		token, err := tokenAuth.Decode(cookie.Value)
+		if err == nil {
+			name, _ := token.Get("name")
+			if name == admin {
+				isLoggedIn = true
+			}
 		}
 	}
-	var title string
-	if isLoggedIn {
-		title = "Chronicle - " + name
-	} else {
-		title = "Chronicle - Login"
-	}
 
-	// Create your page data
 	data := map[string]interface{}{
-		"Title":        title,
+		"Title":        "Chronicle",
 		"IsLoggedIn":   isLoggedIn,
 		"BristolSlice": []int{1, 2, 3, 4, 5, 6, 7},
 	}
 
-	// Render the template with the data
-	err := tmpl.ExecuteTemplate(w, "index.html", data) // assuming your main file is called index.html
+	err := tmpl.ExecuteTemplate(w, "index.html", data)
 	if err != nil {
+		log.Printf("Error rendering template: %+v", err)
 		http.Error(w, "Error rendering template", http.StatusInternalServerError)
-		log.Fatalf(err.Error())
 	}
 }
 
-func handleLogin(users map[string]string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Parse the form data
-		err := r.ParseForm()
-		if err != nil {
-			http.Error(w, "Error parsing form", http.StatusBadRequest)
-			return
-		}
+func handleLogout(w http.ResponseWriter, _ *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Path:     "/",
+		Expires:  time.Unix(0, 0),
+		HttpOnly: true,
+	})
+	w.Header().Set("HX-Redirect", "/")
+	w.Write([]byte("Logged out"))
+}
 
-		// Get the password from the form
-		formPassword := r.Form.Get("Password")
-		password, ok := users[admin]
-		if !ok || password != formPassword {
-			w.Header().Set("Content-Type", "text/html")
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprint(w, `<div class="text-red-500">Login failed. Please try again.</div>`)
-			return
-		}
-
-		_, tokenString, err := tokenAuth.Encode(jwt.MapClaims{
-			"name":  admin,
-			"admin": true,
-			"exp":   time.Now().Add(time.Hour * 72).Unix(),
-		})
-		if err != nil {
-			http.Error(w, "Error encoding token", http.StatusInternalServerError)
-			return
-		}
-
-		cookie := http.Cookie{
-			Name:     "jwt",
-			Value:    tokenString,
-			Expires:  time.Now().Add(72 * time.Hour),
-			HttpOnly: true, // This means the cookie is not accessible via JavaScript
-			Secure:   true, // Use this if your app is served over HTTPS
-			SameSite: http.SameSiteLaxMode,
-			Path:     "/",
-		}
-		http.SetCookie(w, &cookie)
-
-		w.Header().Set("HX-Redirect", "/")
-		fmt.Fprint(w, "Logged in!")
+func handleLogin(w http.ResponseWriter, r *http.Request, users map[string]string) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Printf("Error parsing form: %v", err)
+		http.Error(w, "Error parsing form", http.StatusBadRequest)
+		return
 	}
+	formPassword := r.Form.Get("Password")
+	password, ok := users[admin]
+	if !ok || password != formPassword {
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, `<div class="text-red-500">Login failed. Please try again.</div>`)
+		return
+	}
+
+	_, tokenString, err := tokenAuth.Encode(jwt.MapClaims{
+		"name":  admin,
+		"admin": true,
+		"exp":   time.Now().Add(time.Hour * 72).Unix(),
+	})
+	if err != nil {
+		log.Printf("Error encoding token: %v", err)
+		http.Error(w, "Error encoding token", http.StatusInternalServerError)
+		return
+	}
+
+	cookie := http.Cookie{
+		Name:     "jwt",
+		Value:    tokenString,
+		Expires:  time.Now().Add(72 * time.Hour),
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteLaxMode,
+		Path:     "/",
+	}
+	http.SetCookie(w, &cookie)
+
+	w.Header().Set("HX-Redirect", "/")
+	fmt.Fprint(w, "Logged in!")
 }
 
 func handleMedicineEntry(w http.ResponseWriter, r *http.Request, client *notionapi.Client, medicinePageID string) {
@@ -223,8 +202,8 @@ func handleMedicineEntry(w http.ResponseWriter, r *http.Request, client *notiona
 		Note     string
 	}
 
-	// Parse form data
 	if err := r.ParseForm(); err != nil {
+		log.Printf("Error parsing medicine entry: %v", err)
 		http.Error(w, "Error parsing form data", http.StatusBadRequest)
 		return
 	}
@@ -235,6 +214,7 @@ func handleMedicineEntry(w http.ResponseWriter, r *http.Request, client *notiona
 
 	page, err := notion.AppendMedicineEntry(client, medicinePageID, payload.Medicine, payload.Note)
 	if err != nil {
+		log.Printf("Error appending medicine entry: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -242,6 +222,7 @@ func handleMedicineEntry(w http.ResponseWriter, r *http.Request, client *notiona
 	created := page.CreatedTime.In(loc).Format("2006-01-02 03:04PM")
 	doseProp, ok := page.Properties["Dose"].(*notionapi.TitleProperty)
 	if !ok {
+		log.Printf("Error unwrapping Dose returned from page: %v", err)
 		http.Error(w, "Error unwrapping Dose returned from page", http.StatusInternalServerError)
 		return
 	}
@@ -259,13 +240,14 @@ func handleDigestionEntry(w http.ResponseWriter, r *http.Request, client *notion
 		Note    string
 	}
 
-	// Parse form data
 	if err := r.ParseForm(); err != nil {
+		log.Printf("Error parsing digestion entry: %v", err)
 		http.Error(w, "Error parsing form data", http.StatusBadRequest)
 		return
 	}
 	bristol, err := strconv.Atoi(r.Form.Get("bristol"))
 	if err != nil {
+		log.Printf("Error parsing bristol value: %v", err)
 		http.Error(w, "Invalid value for Bristol", http.StatusBadRequest)
 		return
 	}
@@ -277,6 +259,7 @@ func handleDigestionEntry(w http.ResponseWriter, r *http.Request, client *notion
 
 	page, err := notion.AppendDigestionEntry(client, digestionDbID, payload.Bristol, payload.Size, payload.Note)
 	if err != nil {
+		log.Printf("Error appending digestion entry: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
